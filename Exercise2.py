bf93af9241a4aaa0014f3b09c9ba3a0539e869eb
Pragyaditya Das <pragyaditya.das94@gmail.com>
2014-06-21 20:01:24 +0530

    Python 2.7.6

---
+++ Exercise2.py
@@ -0,0 +1,126 @@
+Python 3.3.2 (v3.3.2:d047928ae3f6, May 16 2013, 00:03:43) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 2+2
+4
+>>> #this is a comment
+>>> ...2+2
+SyntaxError: invalid syntax
+>>> # This is a comment ...2+2
+>>> 2+3
+5
+>>> (50-5*6)/4
+5.0
+>>> 2//2
+1
+>>> 3//2
+1
+>>> 4//5
+0
+>>> 5//4
+1
+>>> 1//10
+0
+>>> 10//9
+1
+>>> 10//3
+3
+>>> 7//3
+2
+>>> 7//-3
+-3
+>>> a = 20
+>>> b = -90
+>>> a*b
+-1800
+>>> q=w=e=r=t=y=0
+>>> q
+0
+>>> w
+0
+>>> e
+0
+>>> r
+0
+>>> t
+0
+>>> y
+0
+>>> y
+0
+>>> t
+0
+r
+>>> 
+>>> 
+>>> e
+0
+>>> e
+0
+>>> w
+0
+>>> w
+0
+>>> s
+Traceback (most recent call last):
+  File "<pyshell#33>", line 1, in <module>
+    s
+NameError: name 's' is not defined
+>>> 1j * 1J
+(-1+0j)
+>>> complex (2,5)
+(2+5j)
+>>> a = complex (3,9)
+>>> 
+>>> a.real
+3.0
+>>> a.imag
+9.0
+>>> float a
+SyntaxError: invalid syntax
+>>> float (a)
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    float (a)
+TypeError: can't convert complex to float
+>>> "jeet"
+'jeet'
+>>> \"jeey
+SyntaxError: unexpected character after line continuation character
+>>> "jtte"\s"
+SyntaxError: unexpected character after line continuation character
+>>> "jtte\"s"
+'jtte"s'
+>>> jeet = "This is me learning python coding\n\ i m using the python docs\n\note thta its a language well known to engineers for assembly language\n\ also hard work in this field is nicessary\and significant."
+>>> print(jeet)
+This is me learning python coding
+\ i m using the python docs
+
+ote thta its a language well known to engineers for assembly language
+\ also hard work in this field is nicessary and significant.
+>>> jet = "This is me learning python coding\n\  i m using the python docs\n\note thta its a language well known to engineers for assembly language\n\ also hard work in this field is nicessary\and significant."
+>>> jet = "This is me learning python coding\n\  i m using the python docs\n\                    note thta its a language well known to engineers for assembly language\n\                        also hard work in this field is nicessary\    and significant."
+>>> print(jet)
+This is me learning python coding
+\  i m using the python docs
+\                    note thta its a language well known to engineers for assembly language
+\                        also hard work in this field is nicessary\    and significant.
+>>> jet = "This is me learning python coding\n  i m using the python docs\n                      note thta its a language well known to engineers for assembly language\n                        also hard work in this field is nicessary\    and significant."
+>>> print(jet)
+This is me learning python coding
+  i m using the python docs
+                      note thta its a language well known to engineers for assembly language
+                        also hard work in this field is nicessary\    and significant.
+>>> jet = "This is me learning python coding\n  i m using the python docs\n                      note thta its a language well known to engineers for assembly language\n                        also hard work in this field is nicessary   and significant."
+>>> print(jet)
+This is me learning python coding
+  i m using the python docs
+                      note thta its a language well known to engineers for assembly language
+                        also hard work in this field is nicessary   and significant.
+>>> 
+
+
+
+
+
+  
+
